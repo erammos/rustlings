196a4555b2db7dc709fe11592185ed3bc2910be5
elias <rammoselias@gmail.com>
2023-12-26 12:10:38 +0200

    19

--- exercises/19_smart_pointers/cow1.rs
+++ exercises/19_smart_pointers/cow1.rs
@@ -48,7 +48,7 @@ mod tests {
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_)=> Ok(())
         }
     }
 
@@ -61,6 +61,7 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Owned(_) => Ok(()),
         }
     }
 
@@ -73,6 +74,7 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Borrowed(_) => Ok(()),
         }
     }
 }
